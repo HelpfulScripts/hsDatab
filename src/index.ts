1daf7c123c05723f44666825652dfd9744a72dac
hs <>
2018-06-01 21:40:04 -0700

    name change to hsDatab

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { NumRange,
+         NumDomain,
+         DateDomain,
+         NameDomain,
+         Domain,
+         ColumnReference,
+         DataVal,
+         DataRow,
+         DataSet
+        }       from './Data';
+
+export { Data } from './Data';
+export { Condition} from './DataFilters';
+
