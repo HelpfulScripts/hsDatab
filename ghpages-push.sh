3bb4a060c15f3adddcdaa2644b5b6344eb7a3d66
hs <>
2018-06-16 18:53:04 +0200

    fixing gh-push

--- ghpages-push.sh
+++ ghpages-push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cp -r docs ../
+cp -R docs ../
 git checkout gh-pages
 cp ../docs/src/* ./src 
 cp ../docs/data/* ./data
+rm -R ../docs
 git commit -a -m 'docs update'
 git push
 git checkout master
